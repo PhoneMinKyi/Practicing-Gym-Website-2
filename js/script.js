f08d95923c789fb344b1f57b676ed2c20fe03a34
Phone Min Kyi <phoneminkyi2222@gmail.com>
2024-10-19 01:01:08 +0630

    Added Footer Section and Responsive Css

--- js/script.js
+++ js/script.js
@@ -25,3 +25,19 @@ addEventOnElem(navTogglers, "click", toggleNavbar);
 const closeNavbar = function() {navbar.classList.remove("active")};
 
 addEventOnElem(navLinks, "click", closeNavbar);
+
+/*Head and Top BTN Active*/ 
+
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
